79d78a7eac5cc0ca6ca9a234e1530b994cad6b16
Bloo00 <kabin.yangg@gmail.com>
2021-12-17 15:04:12 -0800

    i need help

---
+++ src/Showcase.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Showcase.css';
+
+class Showcase extends Component {
+  render() {
+    return (
+    <p>hello world</p>
+
+    )
+  };
+}
+
+export default Showcase;
